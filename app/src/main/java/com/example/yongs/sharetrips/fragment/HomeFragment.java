fb7b02e306d8d5d03dff4bac33a44dac2173e576
sungcheol <32806781+ksc4826@users.noreply.github.com>
2019-05-25 20:59:17 +0900

    Merge master branch

--- app/src/main/java/com/example/yongs/sharetrips/fragment/HomeFragment.java
+++ app/src/main/java/com/example/yongs/sharetrips/fragment/HomeFragment.java
@@ -45,7 +45,6 @@ public class HomeFragment extends Fragment {
     @BindView(R.id.list)
     ListView list;
 
-    SearchView searchView;
     ReportAdapter mReportAdapter;
     ArrayList<Report> mReportArrayList;
     RetrofitReports mRetrofitReports;
@@ -122,7 +121,7 @@ public class HomeFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_search,menu);
-        searchView = (SearchView)menu.findItem(R.id.menu_search).getActionView();
+        SearchView searchView = (SearchView)menu.findItem(R.id.menu_search).getActionView();
         searchView.setMaxWidth(Integer.MAX_VALUE);
         searchView.setQueryHint("검색어를 입력하세요.");
         searchView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
@@ -130,18 +129,16 @@ public class HomeFragment extends Fragment {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                Log.d(TAG, s);
                 mRetrofitReports.getSearch(s, new ApiCallback() {
                     @Override
                     public void onError(Throwable t) {
-                        Log.e(TAG,t.toString());
+
                     }
 
                     @Override
                     public void onSuccess(int code, Object receiveData) {
                         Log.i(TAG, String.valueOf(code));
 
-                        mReportAdapter.reportArrayList.clear();
                         mReportList = (List<Report>) receiveData;
                         for (int i = 0; i < mReportList.size(); i++) {
                             Report report = mReportList.get(i);
@@ -150,17 +147,12 @@ public class HomeFragment extends Fragment {
                             ReportApiService reportApiService = mRetrofitReports.create(ReportApiService.class);
                             Call<ResponseBody> call = reportApiService.getImage(report.getId());
                             new HomeFragment.ImageCall(report).execute(call);
-                            mReportAdapter.reportArrayList.add(report);
                         }
-                        list.setAdapter(mReportAdapter);
-                        mReportAdapter.notifyDataSetChanged();
                     }
 
                     @Override
                     public void onFailure(int code) {
-                        Log.i(TAG,String.valueOf(code));
-                        //mReportAdapter.reportArrayList.clear();
-                        //mReportAdapter.notifyDataSetChanged();
+
                     }
                 });
                 return false;
